7e3029d10cc3b6616c3337df482dfee5b395e841
James Lyon <jameslyon42@gmail.com>
2021-01-02 13:58:42 -0500

    Update pricing

--- components/Button.vue
+++ components/Button.vue
@@ -1,8 +1,30 @@
-<template>
-  <button
-    class="bg-purple text-white font-medium py-2 px-4 rounded-lg text-lg font-spartan"
-    @click="$emit('click')"
-  >
-    <slot></slot>
-  </button>
-</template>
+<script>
+export default {
+  props: {
+    href: {
+      type: String,
+      required: false,
+      default: '',
+    },
+  },
+
+  render(createElement) {
+    return createElement(
+      this.href ? 'a' : 'button',
+      {
+        class:
+          'bg-purple text-white font-medium py-2 px-4 rounded-lg text-lg font-spartan button no-underline cursor-pointer inline-block',
+        on: {
+          click: () => {
+            this.$emit('click')
+          },
+        },
+        attrs: {
+          href: this.href,
+        },
+      },
+      this.$slots.default
+    )
+  },
+}
+</script>
